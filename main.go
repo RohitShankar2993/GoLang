b7fef95a76787d46c20802ce86756e606d746c62
Ubuntu <ubuntu@ip-172-31-2-242.ap-south-1.compute.internal>
2025-06-27 04:07:45 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "fmt"
+  )
+
+func main() {
+     //var num int
+     
+     //fmt.Print("Enter the number:")
+     //fmt.Scan(&num)
+
+     //if num%2==0{
+       //      fmt.Printf("Even %d\n",num)
+     //} else{
+       //      fmt.Printf("Odd %d\n",num)
+     //}
+
+           
+     for {
+	     fmt.Printf("hello\n")
+     }
+
+}
