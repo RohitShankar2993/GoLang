a3b449c96236c6681a1979d41a37c5a199d4f0f4
Ubuntu <ubuntu@ip-172-31-2-242.ap-south-1.compute.internal>
2025-06-27 05:57:28 +0000

    Added Struct Code

--- main.go
+++ main.go
@@ -1,24 +1,26 @@
 package main
 
-import (
-    "fmt"
-  )
+import "fmt"
 
-func main() {
-     //var num int
-     
-     //fmt.Print("Enter the number:")
-     //fmt.Scan(&num)
+func main(){
 
-     //if num%2==0{
-       //      fmt.Printf("Even %d\n",num)
-     //} else{
-       //      fmt.Printf("Odd %d\n",num)
-     //}
+        Rohit := Details{
+                Name: "Rohit Shankar",
+                Email: "rohit123@gmail.com",
+                Address: "Heaven",
+                Age: 20,
+        }
+        fmt.Println("Rohit's Details")
+        fmt.Println("Name:", Rohit.Name)
+        fmt.Println("Email:", Rohit.Email)
+        fmt.Println("Address:", Rohit.Address)
+        fmt.Println("Age:", Rohit.Age)
+        fmt.Printf("%+v",Rohit)
 
-           
-     for {
-	     fmt.Printf("hello\n")
-     }
-
+}
+type Details struct{
+        Name string
+        Email string
+        Address string
+        Age int
 }
